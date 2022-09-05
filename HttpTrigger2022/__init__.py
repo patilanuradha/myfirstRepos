1d9c4b44466745156203d5b39d34bc9d191c1f59
patilanuradha <anuradha.patil@spadeworx.com>
2022-09-05 16:43:26 +0530

    new file added

--- HttpTrigger2022/__init__.py
+++ HttpTrigger2022/__init__.py
@@ -20,5 +20,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            
         )
